db085def54c0011fb2c7156a6189b22ac5b97aec
agapotov <kagapotov@mail.ru>
2023-06-05 14:28:15 +0300

    upd #47 (upd ownership verification success modal view)

--- src/utils/colors.tsx
+++ src/utils/colors.tsx
@@ -4,7 +4,8 @@ export const COLORS = {
     white: '#FFFFFF',
     black: '#000000',
     green: '#52c41a',
-    red: '#ff4d4f'
+    red: '#ff4d4f',
+    grey: 'rgba(120, 120, 120, 1)'
     // blue: '#5E9DDB',
     // purple: '#9C6ED1',
     // pink: '#EA4EC9'
